a9057e470546bacc581bd9f7d5bca1709ce864aa
Yosmar Hinestroza <yosmarpc@gmail.com>
2024-11-26 08:32:43 -0300

    master

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+
+  export const defaultLang = 'en';
+  export const showDefaultLang = true;
+
+  export const ui = {
+
+    en: {
+        'title':  'Welcome to <span class="text-gradient">Astro</span>',
+        'descriptions': 'To get started, open the directory <code>src/pages</code> in your project.<br /> <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message above.',
+        'subcriptions': 'Welcome to Astro'
+    },
+    es: {
+        'title':  'Bienvenido a <span class="text-gradient">Astro</span>',
+        'descriptions': 'To get started, open the directory <code>src/pages</code> in your project.<br /> <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message above.',
+         'subcriptions': 'Bienvenido a Astro'
+    }
+
+  } as const
